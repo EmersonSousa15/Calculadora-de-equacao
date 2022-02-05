cd72e994ab3295e94d39ddd79ccc61601f890e34
EmersonSousa15 <73004348+EmersonSousa15@users.noreply.github.com>
2022-02-05 12:55:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ function explicacao() {
     &Delta; = ${n.b**2} -4 × ${n.a} × ${n.c}<br> 
     &Delta; = ${n.b**2} ${nBaskara.mult} × ${n.c}<br> 
     &Delta; = ${n.b**2} ${nBaskara.mult * n.c}<br> &Delta; = ${valor.d}<br>`
-    if (valor.d > 0 && Number.isInteger(valor.d)) {
+    if (valor.d > 0 && Number.isInteger(valor.rD)) {
     explic.innerHTML += `<h3>Explicação Baskara</h3> 
     <strong> -b +- &radic;&Delta;</strong><br> <strong>X = ------------------------</strong><br> <strong>2a<br> &darr; </strong> <br>
     ${-n.b} +- &radic;${valor.d}<br>
